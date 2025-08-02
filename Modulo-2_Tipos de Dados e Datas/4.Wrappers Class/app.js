cec43bfec702764646a5025185658756a5624202
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-08-02 17:31:54 -0300

    Wra[[er class

---
+++ Modulo-2_Tipos de Dados e Datas/4.Wrappers Class/app.js
@@ -0,0 +1,15 @@
+const s = "abc"; // Tipo primitivo em Javascript "Não tem propriedades, não tem metodos e não tem funções"
+/*
+const v = new String(s);
+const s2 = v.toUpperCase();
+
+[O javascript faz isso implicitamente] Classes wrappers são objetos sua declaração a primeira letra é maiuscula 
+
+/**
+ * string -> String
+ * number -> Number
+ * boolean -> Boolen
+ * */
+
+console.log(s.toUpperCase());
+
