608119f3c8eba64761593855989c8ce7de47c0a1
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-08-02 15:42:37 -0300

    Funções de strings

---
+++ Modulo-2_Tipos de Dados e Datas/3.Funções Impostantes das Strings/app.js
@@ -0,0 +1,27 @@
+//JAVASCRIPIT
+//012345678910
+
+const s1 = "JAVASCRIPIT";
+
+const s2 = s1.slice(1, 9);
+console.log(s2);
+
+const s3 = s1.slice(4, 8);
+console.log(s3);
+
+const tecnologia = "Nodejs";
+
+const t1 = tecnologia.toUpperCase();
+console.log(t1)
+
+const t2 = tecnologia.toLowerCase();
+console.log(t2)
+
+const t3 = tecnologia.length;
+console.log(t3)
+
+let str = "NODEJS";
+
+console.log(str.indexOf("D"));
+
+
