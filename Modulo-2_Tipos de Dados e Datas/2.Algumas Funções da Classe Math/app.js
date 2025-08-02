698a7a53c25df75cc3d9002e542130352c462685
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-08-02 14:39:23 -0300

    Funções Matematicas

---
+++ Modulo-2_Tipos de Dados e Datas/2.Algumas Funções da Classe Math/app.js
@@ -0,0 +1,17 @@
+let v1 = 3.5;
+let v2 = -15;
+let v3 = 81;
+const v4 = 3;
+
+const v5 = Math.max(3.5, 5, 10, 3, 5, 99);
+const v6 = Math.min(3.5, 5, 10, 2, 5, 99);
+
+console.log("=> " + Math.ceil(v1));
+console.log("=> " + Math.floor(v1));
+console.log("=> " + Math.abs(v2));
+console.log("=> " + Math.sqrt(v3));
+console.log("=> " + Math.pow(v4, 2));
+
+console.log(v5);
+console.log(v6);
+
