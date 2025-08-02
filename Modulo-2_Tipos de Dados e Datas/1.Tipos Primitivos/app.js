8f277254c5f70d0447b496a7f4243bf80cd30af4
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-08-02 14:19:55 -0300

    Tipos primitivos

---
+++ Modulo-2_Tipos de Dados e Datas/1.Tipos Primitivos/app.js
@@ -0,0 +1,13 @@
+const v1 = 5;
+const v2 = 3.65;
+const v3 = true;
+const v4 = false;
+const v5 = "ABC"
+const v6 = 'DEF'
+
+console.log("=> " + typeof(v1));
+console.log("=> " + typeof(v2));
+console.log("=> " + typeof(v3));
+console.log("=> " + typeof(v4));
+console.log("=> " + typeof(v5));
+console.log("=> " + typeof(v6));
